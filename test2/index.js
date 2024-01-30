77ed47b749e1ad52e1d5f0b60a44e1a1fa980b65
johnmark287 <johnmark.muhando@students.jkuat.ac.ke>
2024-01-30 23:07:01 +0300

    feat: Get single data from members data file.

--- test2/index.js
+++ test2/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 // Deals with file paths
 const path = require('path');
 
-const moment = require('moment')
+const logger = require('./middleware/logger')
 
 // Import data: members
 const members = require('./Members');
@@ -20,6 +20,16 @@ app.use(logger);
 app.get('/api/members', (req, res) => {
     res.json(members);
 });
+
+// Get single member
+app.get('/api/members/:id', (req, res) => {
+    const found = members.some(member => member.id === parseInt(req.params.id));
+    if(found) {
+        res.json(members.filter(member => member.id === parseInt(req.params.id)));
+    } else {
+        res.status(400).json({ mgs: `No member with id ${req.params.id}` })
+    }
+})
 
 // Set up static folder
 app.use(express.static(path.join(__dirname, 'public')))
