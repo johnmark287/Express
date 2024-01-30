0509a0b39ffd5f624842312a175b53032cb1ecb0
johnmark287 <johnmark.muhando@students.jkuat.ac.ke>
2024-01-30 22:49:53 +0300

    feat: Add JSON test data.

---
+++ test2/Members.js
@@ -0,0 +1,29 @@
+// Array of members
+const members = [
+    {
+        id: 1,
+        name: 'John Doe',
+        email: 'johndoe@gmail.com',
+        status: 'active',
+    },
+    {
+        id: 2,
+        name: 'Bob Shaq',
+        email: 'bobshaq@gmail.com',
+        status: 'inactive',
+    },
+    {
+        id: 3,
+        name: 'Shannon Jackson',
+        email:'shannonjackson@gmail.com',
+        status: 'active',
+    },
+    {
+        id: 4,
+        name: 'Jane Doe',
+        email: 'janedoe@gmail.com',
+        status: 'inactive',
+    }
+]
+
+module.exports = members
