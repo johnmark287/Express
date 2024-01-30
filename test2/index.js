ff8b6ecdca1ed4f02f17a04d6df233ded535a643
johnmark287 <johnmark.muhando@students.jkuat.ac.ke>
2024-01-30 23:09:35 +0300

    feat: Move routes for getting members from index.

--- test2/index.js
+++ test2/index.js
@@ -16,20 +16,7 @@ const app = express();
 app.use(logger);
 
 
-// simple restAPI to get all members
-app.get('/api/members', (req, res) => {
-    res.json(members);
-});
 
-// Get single member
-app.get('/api/members/:id', (req, res) => {
-    const found = members.some(member => member.id === parseInt(req.params.id));
-    if(found) {
-        res.json(members.filter(member => member.id === parseInt(req.params.id)));
-    } else {
-        res.status(400).json({ mgs: `No member with id ${req.params.id}` })
-    }
-})
 
 // Set up static folder
 app.use(express.static(path.join(__dirname, 'public')))
